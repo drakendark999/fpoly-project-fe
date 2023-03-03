7d440c8158e310ee1920c8e4542d343976dc3d8a
nghienhao <91198089+nghienhao@users.noreply.github.com>
2023-03-03 18:05:44 +0700

    Comment Code

--- server/src/routes/api/lichThi2.js
+++ server/src/routes/api/lichThi2.js
@@ -11,31 +11,31 @@ lichThi2Router.post("/", async (req, res) => {
     let data = req.body;
 
     // Chuyển hết dữ liệu từ lịch thi 2 sang lịch thi
-    lichThi2.findAll().then(data => {
-      data.forEach((row) => {
-        lichThi.findOrCreate({
-          where: {
-            maLich_Thi: row.dataValues.maLich_Thi,
-          },
-          defaults: {
-            maKy_Thi: row.dataValues.maKy_Thi,
-            idKhu_Vuc: row.dataValues.idKhu_Vuc,
-            idToa_Nha: row.dataValues.idToa_Nha,
-            ten_Phong: row.dataValues.ten_Phong,
-            ma_Lop: row.dataValues.ma_Lop,
-            ma_Mon: row.dataValues.ma_Mon,
-            bo_Mon: row.dataValues.bo_Mon,
-            dot_Thi: row.dataValues.dot_Thi,
-            ngay_Thi: row.dataValues.ngay_Thi,
-            ca_Thi: row.dataValues.ca_Thi,
-            so_SV: row.dataValues.so_SV,
-            maLich_Thi: row.dataValues.maLich_Thi,
-            GV1: row.dataValues.GV1,
-            GV2: row.dataValues.GV2,
-            status: row.dataValues.status
-          }
-      })
-    })});
+    // lichThi2.findAll().then(data => {
+    //   data.forEach((row) => {
+    //     lichThi.findOrCreate({
+    //       where: {
+    //         maLich_Thi: row.dataValues.maLich_Thi,
+    //       },
+    //       defaults: {
+    //         maKy_Thi: row.dataValues.maKy_Thi,
+    //         idKhu_Vuc: row.dataValues.idKhu_Vuc,
+    //         idToa_Nha: row.dataValues.idToa_Nha,
+    //         ten_Phong: row.dataValues.ten_Phong,
+    //         ma_Lop: row.dataValues.ma_Lop,
+    //         ma_Mon: row.dataValues.ma_Mon,
+    //         bo_Mon: row.dataValues.bo_Mon,
+    //         dot_Thi: row.dataValues.dot_Thi,
+    //         ngay_Thi: row.dataValues.ngay_Thi,
+    //         ca_Thi: row.dataValues.ca_Thi,
+    //         so_SV: row.dataValues.so_SV,
+    //         maLich_Thi: row.dataValues.maLich_Thi,
+    //         GV1: row.dataValues.GV1,
+    //         GV2: row.dataValues.GV2,
+    //         status: row.dataValues.status
+    //       }
+    //   })
+    // })});
 
     // Xóa hết dữ liệu lịch thi 2 
     await lichThi2.destroy({where: {}})
